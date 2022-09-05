e65ff9e2f5c932c679ff4063ceada61dced00c03
veronicaglh <veronicagt.IT@gmail.com>
2022-09-05 19:14:07 +0300

    first commit

---
+++ ExerciseOne.c
@@ -0,0 +1,10 @@
+/*This program will contain different exercises that have been done with C*/
+/*The program solves the exercises taken from the book C Programming: A modern approach book,*/
+/*Questions are taken from Chapter 6 - Programming Projects (questions 1,2,3 and 11)*/
+/*To compile this program in Ubuntu or other Linux Distributions you run the command: gcc -o ExerciseOne ExerciseOne.c */
+/*Make sure you are in the correct folder path before doing so*/
+/*To run the program you can use the command: ./ExerciseOne */
+
+/*The first exercise will prompt the user to enter numbers one by one.*/
+/*When the user enters 0 or a negative number the program will display the largest number entered */
+/*and display the greatest as well as smallest integer*/
