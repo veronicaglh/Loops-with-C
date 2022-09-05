1a6e7aef560d803899970e7b17f0763815ac618b
veronicaglh <veronicagt.IT@gmail.com>
2022-09-05 19:50:54 +0300

    solve the first exercise

--- ExerciseOne.c
+++ ExerciseOne.c
@@ -8,3 +8,32 @@
 /*The first exercise will prompt the user to enter numbers one by one.*/
 /*When the user enters 0 or a negative number the program will display the largest number entered */
 /*and display the greatest as well as smallest integer*/
+
+#include<stdio.h>
+int main(){
+
+    /*Two variables have been declared here.*/
+    /*The num variable will be used to store the greatest number the user inputs*/
+    /*intialValue will be used to store the number the user inputs(not necessarily the largest number but whatever the user inputs)*/
+
+    int num = 0;
+    int intialValue;
+
+    /*This for loop will only be displayed when the user inputs a number besides 0 or negative value*/
+    /*As soon as we run the program this loop will be displayed and thats because intialValue has a value different from 0*/
+    /*The loop recieves a number from user(intialValue) and comapres it with num(0). If the value the user inputs is greater than num. It stores the greater value in num*/
+    
+    for (int i = 0; intialValue > 0; i++){ 
+        printf("Please enter the number: ");
+        scanf("%d", &intialValue);
+        num = num>intialValue ? num:intialValue;
+        }
+
+    
+    /*This is only dispalyed when the user enters a number thats less than or equal to zero*/
+    /*num contains the greatest number.*/
+    
+    printf("The greatest number is: %d.\n",num);
+    
+     return 0;
+}   
